bcfe8348b76060a2e6683f212aab20e060d25acd
Kiev <kevinkiss2010@gmail.com>
2024-07-11 19:38:51 -0300

    Tipando e att projeto

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,10 @@
-import { Negociacao } from "./models/negociacao";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(),10,100);
-console.log(negociacao);
-alert('oi');
- 
+
+const controller = new NegociacaoController(); // Adicione os parênteses aqui
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona(); // Invocação correta do método adiciona()
+});
